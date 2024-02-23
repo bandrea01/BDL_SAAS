e407bd5e0c15eb6132601f0fd893c3e6ef7d37c5
mirkocaforio <mirkocaforio2002@gmail.com>
2024-02-23 12:08:14 +0100

    Update py2arango.py

--- flask_root/src/py2arango.py
+++ flask_root/src/py2arango.py
@@ -7,7 +7,7 @@ import ifcopenshell
 # Create the basic node with literal attributes and the class hierarchy
 # Input: ifc_entity - an instance, ifc_file - the parsed ifc-SPF
 # Output: basic nodes with properties for literal attributes and labels for ifc class hierarchy
-def create_pure_node_from_ifc_entity(ifc_entity, ifc_file, hierarchy=True):
+def create_pure_node_from_ifc_entity(ifc_entity):
     node = {'_key': str(ifc_entity.id() if ifc_entity.id() != 0 else uuid4()), 'name': ifc_entity.is_a()}
     attributes_type = ['ENTITY INSTANCE', 'AGGREGATE OF ENTITY INSTANCE', 'DERIVED']
     for i in range(ifc_entity.__len__()):
@@ -25,7 +25,7 @@ existing_edges = set()
 # Input: db - a link to ArangoDB graph database, ifc_entity - an instance, ifc_file - the parsed ifc-SPF
 # Output: a subgraph
 def create_graph_from_ifc_entity_all(db, ifc_entity, ifc_file, nodes_name, edges_name):
-    node = create_pure_node_from_ifc_entity(ifc_entity, ifc_file)
+    node = create_pure_node_from_ifc_entity(ifc_entity)
     collection = db[nodes_name]
 
     # Check if node already exists in the database
@@ -39,7 +39,7 @@ def create_graph_from_ifc_entity_all(db, ifc_entity, ifc_file, nodes_name, edges
                 if ifc_entity.is_a() in ['IfcOwnerHistory'] and ifc_entity.is_a() != 'IfcProject':
                     continue
                 else:
-                    sub_node = create_pure_node_from_ifc_entity(ifc_entity[i], ifc_file)
+                    sub_node = create_pure_node_from_ifc_entity(ifc_entity[i])
                     sub_collection = db[nodes_name]
 
                     # Check if sub node already exists in the database
@@ -52,15 +52,15 @@ def create_graph_from_ifc_entity_all(db, ifc_entity, ifc_file, nodes_name, edges
                     if edge_key not in existing_edges:
                         rel_collection = db[edges_name]
                         rel_collection.insert({
-                            "_from": nodes_name + "/" + node["_key"],
-                            "_to": nodes_name + "/" + sub_node["_key"],
+                            "_from": f"{nodes_name}/{node['_key']}",
+                            "_to": f"{nodes_name}/{sub_node['_key']}",
                             "rel_name": ifc_entity.wrapped_data.get_argument_name(i)
                         })
                         existing_edges.add(edge_key)
 
             elif ifc_entity.wrapped_data.get_argument_type(i) == 'AGGREGATE OF ENTITY INSTANCE':
                 for sub_entity in ifc_entity[i]:
-                    sub_node = create_pure_node_from_ifc_entity(sub_entity, ifc_file)
+                    sub_node = create_pure_node_from_ifc_entity(sub_entity)
                     sub_collection = db[nodes_name]
 
                     # Check if sub node already exists in the database
@@ -73,8 +73,8 @@ def create_graph_from_ifc_entity_all(db, ifc_entity, ifc_file, nodes_name, edges
                     if edge_key not in existing_edges:
                         rel_collection = db[edges_name]
                         rel_collection.insert({
-                            "_from": nodes_name + "/" + node["_key"],
-                            "_to": nodes_name + "/" + sub_node["_key"],
+                            "_from": f"{nodes_name}/{node['_key']}",
+                            "_to": f"{nodes_name}/{sub_node['_key']}",
                             "rel_name": ifc_entity.wrapped_data.get_argument_name(i)
                         })
                         existing_edges.add(edge_key)
@@ -84,8 +84,9 @@ def create_graph_from_ifc_entity_all(db, ifc_entity, ifc_file, nodes_name, edges
             inverse_relations = ifc_entity.wrapped_data.get_inverse(rel_name)
             for wrapped_rel in inverse_relations:
                 rel_entity = ifc_file.by_id(wrapped_rel.id())
-                sub_node = create_pure_node_from_ifc_entity(rel_entity, ifc_file)
+                sub_node = create_pure_node_from_ifc_entity(rel_entity)
                 sub_collection = db[nodes_name]
+
                 # Check if sub node already exists in the database
                 existing_sub_node = sub_collection.get(sub_node["_key"])
                 if not existing_sub_node:
@@ -96,8 +97,8 @@ def create_graph_from_ifc_entity_all(db, ifc_entity, ifc_file, nodes_name, edges
                 if edge_key not in existing_edges:
                     rel_collection = db[edges_name]
                     rel_collection.insert({
-                        "_from": nodes_name + "/" + sub_node["_key"],
-                        "_to": nodes_name + "/" + sub_node["_key"],
+                        "_from": f"{nodes_name}/{sub_node['_key']}",
+                        "_to": f"{nodes_name}/{sub_node['_key']}",
                         "rel_name": rel_name
                     })
                     existing_edges.add(edge_key)
@@ -113,6 +114,7 @@ def create_full_graph(db, ifc_file, nodes_name, edges_name):
         idx += 1
     return
 
+
 if len(sys.argv) != 2:
     print("Usage: python py2arango.py <path_to_ifc_file>")
     sys.exit(1)
@@ -122,9 +124,9 @@ client = ArangoClient(hosts='http://bdl_saas-arangodb-1:8529')
 db = client.db('prova', username='root', password='BDLaaS')
 
 filename = sys.argv[1].rsplit('/', 1)[-1].split('.')[-2]
-nodes_name = filename + '_nodes'
-edges_name = filename + '_edges'
-graph_name = filename + '_graph'
+nodes_name = f"{filename}_nodes"
+edges_name = f"{filename}_edges"
+graph_name = f"{filename}_graph"
 
 if not (db.has_collection(nodes_name) and db.has_collection(edges_name)):
     db.create_collection(nodes_name)
